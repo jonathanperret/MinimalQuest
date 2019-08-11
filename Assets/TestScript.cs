de03b3ab578350ffa37ec13c8d02222ba2aecfbf
Jonathan Perret <j-github@jonathanperret.net>
2019-08-11 19:30:36 +0200

    Basic Android Pancake

---
+++ Assets/TestScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogFormat("Tier={0}", Graphics.activeTier);
+    }
+}
