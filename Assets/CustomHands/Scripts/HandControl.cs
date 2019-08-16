9072c1460a04489cdcf87edf5ebd6e6d1dd7b26c
Jonathan Perret <j-github@jonathanperret.net>
2019-08-16 23:18:51 +0200

    New exported hands

--- Assets/CustomHands/Scripts/HandControl.cs
+++ Assets/CustomHands/Scripts/HandControl.cs
@@ -1,15 +1,8 @@
-﻿/********************************************************************************//**
-\file      Hand.cs
-\brief     Basic hand impementation.
-\copyright Copyright 2015 Oculus VR, LLC All Rights reserved.
-************************************************************************************/
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-public class HandDemo : MonoBehaviour
+public class HandControl : MonoBehaviour
 {
     public const string ANIM_LAYER_NAME_POINT = "Point Layer";
     public const string ANIM_LAYER_NAME_THUMB = "Thumb Layer";
@@ -17,12 +10,12 @@ public class HandDemo : MonoBehaviour
     public const string ANIM_PARAM_NAME_POSE = "Pose";
     public const float THRESH_COLLISION_FLEX = 0.9f;
 
-    private Animator m_animator = null;
-
     private int m_animLayerIndexThumb = -1;
     private int m_animLayerIndexPoint = -1;
     private int m_animParamIndexFlex = -1;
     private int m_animParamIndexPose = -1;
+
+    public Animator m_animator = null;
 
     [Range(0, 10)]
     public int m_pose;
@@ -32,7 +25,10 @@ public class HandDemo : MonoBehaviour
 
     private void Awake()
     {
-        m_animator = GetComponent<Animator>();
+        if (m_animator == null)
+        {
+            m_animator = GetComponent<Animator>();
+        }
     }
 
     private void Start()
@@ -45,11 +41,6 @@ public class HandDemo : MonoBehaviour
     }
 
     private void Update()
-    {
-        UpdateAnimStates();
-    }
-
-    private void UpdateAnimStates()
     {
         m_animator.SetInteger(m_animParamIndexPose, m_pose);
 
