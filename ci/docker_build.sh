a407b685fa297cac677681a96e42a4c3c5e6db72
Jonathan Perret <j-github@jonathanperret.net>
2019-11-17 23:30:00 +0100

    Improve docker_build.sh local test script

--- ci/docker_build.sh
+++ ci/docker_build.sh
@@ -2,13 +2,22 @@
 
 set -e
 
+: ${UNITY_LICENSE_CONTENT:?}
+: ${BUILD_NAME:?}
+: ${BUILD_TARGET:?}
+: ${IMAGE_NAME:?}
+
+mkdir -p Temp
+tar czf Temp/project.tgz Assets/ ProjectSettings/ ci/
+
 docker run \
-  -e BUILD_NAME \
+  --rm -ti \
   -e UNITY_LICENSE_CONTENT \
+  -e BUILD_NAME \
   -e BUILD_TARGET \
-  -e UNITY_USERNAME \
-  -e UNITY_PASSWORD \
   -w /project/ \
-  -v $(pwd):/project/ \
+  -v "$(pwd)"/Temp/project.tgz:/project.tgz:ro \
+  -v "$(pwd)"/Builds:/project/Builds \
+  -v "$(basename "$(pwd)")_unity_cache":/project/Library \
   $IMAGE_NAME \
-  /bin/bash -c "/project/ci/before_script.sh && /project/ci/build.sh"
+  /bin/bash -c "tar xzf /project.tgz && /project/ci/before_script.sh && /project/ci/build.sh"
